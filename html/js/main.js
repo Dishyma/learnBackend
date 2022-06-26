6c780ccdd25d7445bc983e92c3b44e48ee3e96bd
Dishyma <marojas4126@gmail.com>
2022-06-26 12:44:32 -0400

    add links tags, ids and classes, js file

---
+++ html/js/main.js
@@ -0,0 +1,8 @@
+// asi se llama desde js a html
+const box = document.querySelector('.box');
+
+box.style.width = "100px"
+box.style.height = "100px"
+box.style.border = "1px solid black"
+box.style.backgroundColor = "red"
+box.style.color = "white"
